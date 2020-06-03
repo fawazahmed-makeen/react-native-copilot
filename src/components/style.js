cd5448773cbff8d8001b4e836ec45865300798ce
Fawaz Ahmed <55703505+fawazahmed-makeen@users.noreply.github.com>
2020-06-03 06:36:16 +0500

    Update style.js

--- src/components/style.js
+++ src/components/style.js
@@ -70,7 +70,7 @@ export default StyleSheet.create({
   },
   overlayRectangle: {
     position: 'absolute',
-    // backgroundColor: 'rgba(0,0,0,0.2)',
+    backgroundColor: 'black',
     left: 0,
     top: 0,
     bottom: 0,
